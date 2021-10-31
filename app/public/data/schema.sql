27fcc61eeaa052bb60d7aae044939e73fc1c2492
Avni Sharma <16avnisharma@gmail.com>
2021-10-31 00:10:38 -0400

    Update schema.sql

--- app/public/data/schema.sql
+++ app/public/data/schema.sql
@@ -1,23 +1,65 @@
-CREATE DATABASE IF NOT EXISTS msisdb;
-USE msisdb;
+CREATE DATABASE IF NOT EXISTS finaldb;
+USE finaldb;
+
+DROP TABLE IF EXISTS referee;
+
+DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE referee(id INT NOT NULL PRIMARY KEY auto_increment, 
+
+                    firstname VARCHAR(25) NOT NULL, lastname VARCHAR(25) NOT NULL, grade VARCHAR(25) NOT NULL,
+
+                     Age INT, Referee_Skill INT, Referee_Status VARCHAR(25));
 
 
-DROP TABLE IF EXISTS Book;
-CREATE TABLE `Book` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `Title` VARCHAR(200) NULL,
-  `Author` VARCHAR(100) NULL,
-  `Year_Published` VARCHAR(4) NULL,
-  `Publisher` VARCHAR(100) NULL,
-  `Page_Count` INT NULL,
-  `MSRP` DOUBLE NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE game(id INT NOT NULL PRIMARY KEY auto_increment, 
+field varchar(100), 
+gDate DATE
+gtime TIME DEFAULT CURRENT_TIMESTAMP);
 
 
-INSERT INTO Book ( Title, Author, Year_Published,Publisher,Page_Count, MSRP ) VALUES 
-('Fundamentals of Wavelets','Goswami, Jaideva',1960,'Wiley',244,100),
-('Data Smart','Foreman, John',1978,'Wiley',267,103),
-('God Created the Integers','Hawking, Stephen',1950,'Penguin',197,75),
-('Data Scientists at Work','Sebastian Gutierrez',1947,'Apress',230,65 ),
-('Silverview','John le Carré',1999,'Oreilly',221,45);
+-- A report can be generated with joins
+-- CREATE TABLE report(id INT NOT NULL PRIMARY KEY auto_increment, 
+-- Game_ID INT, Game_Date DATE, Assigned_Referee TEXT);
+
+CREATE TABLE user (id INT NOT NULL PRIMARY KEY auto_increment, assignor BOOLEAN);
+-- if user is assignor then show skill and status else name,age, grade
+
+
+INSERT INTO referee VALUES
+
+     ('Kevin','Kim', 'National', 32,70,'Unassigned'),
+
+     ('Herbert', 'Miles','Local', 40,80, 'Unassigned'),
+
+     ('Morris', 'Reed', 'International', 35,40, 'Unassigned'),
+
+     ('Paula', 'Edwards', 'National',45, 70, 'Unassigned'),
+
+     ('Tom', 'Gregory', 'Local', 50, 100, 'Unassigned');
+
+
+
+INSERT INTO game VALUES
+
+    ('field1','2021-11-01'),
+
+    ('field2', '2021-11-02'),
+
+    ('field3', '2021-11-04'),
+
+    ('field4', '2021-11-06');
+
+
+
+-- INSERT INTO Report VALUES
+
+--      (1, 101, '2021-11-01', 'Kevin Kim'),
+
+--      (2, 102, '2021-11-10', 'Herbert Miles'),
+
+--      (3, 103, '2021-11-13', 'Morris Read'),
+
+--      (4, 104, '2021-12-11', 'Tom Gregory');     
 
