53911e8ef2f010c4369c6450acbe69e336ae4220
Avni Sharma <16avnisharma@gmail.com>
2021-11-06 01:56:46 -0400

    update report 1

---
+++ app/public/api/reports/unassignedfuturegames.php
@@ -0,0 +1,67 @@
+<?php
+
+try {
+    $_POST = json_decode(
+                file_get_contents('php://input'), 
+                true,
+                2,
+                JSON_THROW_ON_ERROR
+            );
+} catch (Exception $e) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request");
+    exit;
+}
+
+require("class/DbConnection.php");
+
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+        'SELECT a.assign_status, 
+        g.field,
+        g.gdate, 
+        g.gtime, 
+        r.firstname,
+        r.lastname
+        FROM game g, referee r, assign a 
+        WHERE r.id = a.refereeid AND 
+              g.id = a.gameid AND 
+              g.gdate > ? and a.assign_status = "unassigned"');       
+
+
+
+$stmt->execute([
+  $_POST['gdate'],
+  
+ ]);
+
+ $games = $stmt->fetchAll();
+ if (isset($_GET['format']) && $_GET['format']=='csv') {
+    header('Content-Type: text/csv');
+    echo "Referee Status,Game Field,Game Date,Start Time, End Time\r\n";
+  
+    foreach($games as $g) {
+      echo "\"".$g['referee_status'] . "\","
+          .$g['game_field'] . ","
+          .$g['game_date'] . ","
+          .$g['start_time'] . ","
+          .$g['end_time']."\r\n";
+    }
+  
+  } 
+
+ // Step 3: Convert to JSON
+ else {
+     $json = json_encode($games, JSON_PRETTY_PRINT);
+ 
+ // Step 4: Output
+ header('Content-Type: application/json');
+ echo $json;
+ }
---
+++ app/public/js/report.js
@@ -0,0 +1,77 @@
+const reportsApp = {
+    data() {
+      return {
+        games: [],
+        referees : [],
+        gamesForm: {},
+        gameReportsForm : {},
+            
+      
+      }
+    },
+    computed: {},
+    methods: {
+        UnassignedFutureGames(evt){
+
+            fetch('api/reports/unassignedfuturegames.php', {
+                method:'POST',
+                body: JSON.stringify(this.gamesForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.games = json;
+                
+              
+              })
+              .catch( err => {
+                alert("Something went wrong.");
+              });
+          },
+          fetchRefereeData() {
+            fetch('/api/referee/')
+            .then( response => response.json() )
+              .then( (responseJson) => {
+                  console.log(responseJson);
+                  this.referees = responseJson;
+              })
+              .catch( err => {
+                console.error("ERROR", err);
+              });
+          },
+          listGamesDateRange(evt){
+             fetch('api/report/listGamesDateRange.php', {
+                method:'POST',
+                body: JSON.stringify(this.gameReportsForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.games = json;
+                
+              
+              })
+              .catch( err => {
+                alert("Oops, we have an error. Can you try again with correct values.");
+              });
+          }, 
+
+    },
+    created() {
+        this.fetchRefereeData(); 
+    }
+   
+  
+  }
+  
+  Vue.createApp(reportsApp).mount('#reportjs');
+  
+  // Resources : https://stackoverflow.com/questions/62975890/how-to-reload-refresh-a-webpage-in-vue-js-after-a-certain-event-or-a-button-is-c
